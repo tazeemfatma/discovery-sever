5961e945ea66bec2bddd235dce8ad4fd2f0556b8
Tazeem Fatma <Tazeem.Fatma@BRILLIO.COM>
2023-10-04 17:31:30 +0530

    Initial commit

---
+++ src/test/java/com/example/discoverysever/DiscoverySeverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.discoverysever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoverySeverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
